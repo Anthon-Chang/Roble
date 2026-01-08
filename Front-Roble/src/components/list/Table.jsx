bc971035cd58b5ad9731708f00af7e285d59679c
Roger Grefa <kaell.room.@gmail.com>
2026-01-08 00:29:20 -0500

    update

--- Front-Roble/src/components/list/Table.jsx
+++ Front-Roble/src/components/list/Table.jsx
@@ -1,8 +1,31 @@
 import { MdDeleteForever, MdInfo, MdPublishedWithChanges } from "react-icons/md";
 import { useFetch } from "../../hooks/useFetch";
 import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router';
+import { ToastContainer } from "react-toastify";
 
 const Table = () => {
+
+  const deleteCliente = async(id) => {
+        const confirmDelete = confirm("Vas registrar la salida del paciente, ¿Estás seguro?")
+        if(confirmDelete){
+            const url = `${import.meta.env.VITE_BACKEND_URL}/paciente/eliminar/${id}`
+            const storedUser = JSON.parse(localStorage.getItem("auth-token"))
+            const options = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${storedUser.state.token}`,
+                }
+            }
+            const data ={
+                salidaMascota:new Date().toString()
+            }
+            await fetchDataBackend(url, data, "DELETE", options.headers)
+            setPatients((prevPatients) => prevPatients.filter(patient => patient._id !== id))
+        }
+    }
+
+  const navigate = useNavigate()
   const fetchDataBackend = useFetch();
   const [patients, setPatients] = useState([]);
   const [loading, setLoading] = useState(true); // Para manejar estado de carga
@@ -10,7 +33,7 @@ const Table = () => {
 
   const listclientes = async () => { 
     try {
-      const url = `${import.meta.env.VITE_BACKEND_URL}/listar`;
+      const url = `${import.meta.env.VITE_BACKEND_URL}/api/proyecto/listar`;
       const storedUser = JSON.parse(localStorage.getItem("auth-token"));
 
       if (!storedUser?.state?.token) {
@@ -70,10 +93,11 @@ const Table = () => {
 
   return (
     <table className="w-full mt-5 table-auto shadow-lg bg-white">
+      <ToastContainer/>
       {/* Encabezado */}
       <thead className="bg-gray-800 text-slate-400">
         <tr>
-          {["N°", "Nombre mascota", "Nombre del cliente", "Email", "Celular", "Estado", "Acciones"].map((header) => (
+          {["N°", "Producto", "Nombre del cliente", "Email", "Celular", "Precio", "Fecha de entrega","Estado","Acciones"].map((header) => (
             <th key={header} className="p-2">
               {header}
             </th>
@@ -85,22 +109,24 @@ const Table = () => {
       <tbody>
         {patients.map((cliente, index) => (
           <tr className="hover:bg-gray-300 text-center" key={cliente._id}>
-            <td>{index + 1}</td>
-            <td>{cliente.nombreProducto}</td>
-            <td>{cliente.nombreCliente}</td>
-            <td>{cliente.emailCliente}</td>
-            <td>{cliente.celularCliente}</td>
-            <td>
-              <span
-                className={`text-xs font-medium mr-2 px-2.5 py-0.5 rounded ${
-                  cliente.estadoMascota
-                    ? "bg-blue-100 text-green-500 dark:bg-blue-900 dark:text-blue-300"
-                    : "bg-gray-100 text-gray-500 dark:bg-gray-700 dark:text-gray-400"
-                }`}
-              >
-                {cliente.estadoMascota ? "Activo" : "Inactivo"}
-              </span>
-            </td>
+              <td>{index + 1}</td>
+              <td className="px-2 py-1 text-left">{cliente.nombreProyecto}</td>
+              <td className="px-2 py-1">{cliente.nombreCliente}</td>
+              <td className="px-2 py-1">{cliente.emailCliente}</td>
+              <td className="px-2 py-1">{cliente.celularCliente}</td>
+              <td className="px-2 py-1">{cliente.precioProyecto ?? 0}</td>
+              <td className="px-2 py-1">{cliente.fechaEntrega ? new Date(cliente.fechaEntrega).toLocaleDateString() : "-"}</td>
+              <td>
+                <span
+                  className={`text-xs font-medium mr-2 px-2.5 py-0.5 rounded ${
+                    cliente.estadoProyecto
+                      ? "bg-blue-100 text-green-500 dark:bg-blue-900 dark:text-blue-300"
+                      : "bg-gray-100 text-gray-500 dark:bg-gray-700 dark:text-gray-400"
+                  }`}
+                >
+                  {cliente.estadoProyecto ? "Activo" : "Inactivo"}
+                </span>
+              </td>
             <td className="py-2 text-center">
               <MdPublishedWithChanges
                 title="Actualizar"
@@ -109,10 +135,12 @@ const Table = () => {
               <MdInfo
                 title="Más información"
                 className="h-7 w-7 text-slate-800 cursor-pointer inline-block mr-2 hover:text-green-600"
+                onClick={() => navigate(`/dashboard/details/${cliente._id}`)}
               />
               <MdDeleteForever
                 title="Eliminar"
                 className="h-7 w-7 text-red-900 cursor-pointer inline-block hover:text-red-600"
+                onClick={()=>{deleteCliente(cliente._id)}}
               />
             </td>
           </tr>
