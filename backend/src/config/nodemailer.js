c060dd1be4c301a1d5b75f073c8e60638807a3ba
Roger Grefa <kaell.room@gmail.com>
2026-01-14 15:11:22 -0500

    update

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -4,7 +4,9 @@ dotenv.config();
 
 // Config Gmail
 const gmailConfig = {
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASS,
