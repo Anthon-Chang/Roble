f8899b800fcb3a028defc1d0e9c2c9528e73742e
DiegoM-Hack <santitodiego12@gmail.com>
2026-01-25 13:14:33 -0500

    cors3

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 
 app.use(cors({
   origin: process.env.NODE_ENV === "production"
-    ? "https://roble.netlify.app"
+    ? "https://rob1e.netlify.app"
     : true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
