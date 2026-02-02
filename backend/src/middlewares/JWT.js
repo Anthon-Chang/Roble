f2767c33362d0ba627af9e5a9c35cd01fa7d6286
DiegoM-Hack <santitodiego12@gmail.com>
2026-02-02 09:10:17 -0500

    boton de google

--- backend/src/middlewares/JWT.js
+++ backend/src/middlewares/JWT.js
@@ -1,51 +1,55 @@
 import jwt from "jsonwebtoken"
-import carpintero from "../models/carpintero.js"
+import Carpintero from "../models/carpintero.js"
 import Proyecto from "../models/Proyecto.js"
 
 // =============================
 // CREAR TOKEN
 // =============================
 const crearTokenJWT = (id, rol) => {
-    return jwt.sign({ id, rol }, process.env.JWT_SECRET, { expiresIn: "1d" })
+  return jwt.sign({ id, rol }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  })
 }
 
 // =============================
 // VERIFICAR TOKEN
 // =============================
 const verificarTokenJWT = async (req, res, next) => {
+  const authHeader = req.headers.authorization
 
-    const { authorization } = req.headers
-    if (!authorization) {
-        return res.status(401).json({ msg: "Acceso denegado: token no proporcionado" })
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      msg: "Acceso denegado: token no proporcionado",
+    })
+  }
+
+  try {
+    const token = authHeader.split(" ")[1]
+    const { id, rol } = jwt.verify(token, process.env.JWT_SECRET)
+
+    let user
+
+    if (rol === "carpintero") {
+      user = await Carpintero.findById(id).select("-password")
+    } else {
+      user = await Proyecto.findById(id).select("-passwordCliente")
     }
-    try {
-        const token = authorization.split(" ")[1]
-        const { id, rol } = jwt.verify(token, process.env.JWT_SECRET)
-        if (rol === "carpintero") {
-            const carpinteroBDD = await carpintero.findById(id).lean().select("-password")
-            if (!carpinteroBDD) {
-                return res.status(401).json({ msg: "Usuario no encontrado" })
-            }
-            req.carpinteroHeader = carpinteroBDD
-            next()
-        }
-       else {
-            const clienteBDD = await Proyecto.findById(id).lean().select("-passwordCliente");
-            if (!clienteBDD) {
-                return res.status(401).json({ msg: "Usuario no encontrado" });
-            }
-            req.clienteHeader = clienteBDD;
-            next();
-        }
 
-} catch (error) {
-    console.log(error)
-    return res.status(401).json({ msg: `Token inválido o expirado - ${error}` })
-}
+    if (!user) {
+      return res.status(401).json({ msg: "Usuario no encontrado" })
+    }
 
+    req.user = user
+    req.user.rol = rol
+
+    next()
+  } catch (error) {
+    console.error(error)
+    return res.status(401).json({ msg: "Token inválido o expirado" })
+  }
 }
 
 export {
-crearTokenJWT,
-verificarTokenJWT
+  crearTokenJWT,
+  verificarTokenJWT
 }
